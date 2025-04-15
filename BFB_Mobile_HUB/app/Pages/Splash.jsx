d41825990d7ae15529a6c377e052bd9e45c43257
tofu11 <87494814+tofu11@users.noreply.github.com>
2025-04-15 18:38:20 -0400

    initial commit

---
+++ BFB_Mobile_HUB/app/Pages/Splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SplashScreen = () => (
+<div className="splash_screen #fb8500 0%, #fff 100%)] flex flex-col justify-end items-center pt-[23.5rem] pb-[6.6875rem] pl-[3.5625rem] pr-[3.5625rem] w-[393px] rounded-[50px] bg-[linear-gradient(180deg,">
+  <div className="flex justify-center items-center py-4 px-0 w-[17.4375rem] h-12 rounded-full bg-[#ffb703] text-white text-center font-['Inter'] font-medium">
+    Begin
+  </div>
+  <div className="bridges_from_broders text-white text-center font-['Nunito'] text-[3.125rem] font-extrabold leading-5">Bridges
+    From 
+    Broders</div>
+</div>
+);
+
+export default SplashScreen;
